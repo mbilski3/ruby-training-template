ba15b20efd61d2c0aceba06dd3c3b58820b6cd5b
Mateusz Bilski <mateusz.bilski@contractors.roche.com>
2017-12-19 16:38:45 +0100

    Add alias for fast cd to app dir

--- lib/controllers/main_controller.rb
+++ lib/controllers/main_controller.rb
@@ -6,11 +6,6 @@ class MainController < BaseController
     haml :index
   end
 
-  get '/data.json' do
-    content_type 'application/json'
-    JSON({x: 5})
-  end
-
   get '/set' do
     @zmienna = 6
     haml :index
@@ -18,17 +13,11 @@ class MainController < BaseController
 
   get '/posts' do
     @post = Post.new('mbilski3', 'This is my first post')
-    sd
+    byebug
     haml :posts
   end
 
   get '/posts/:author' do |author|
     haml :post_show
-  end
-
-  post '/update' do
-    # This block modifies something
-    content_type :json
-    {status: :ok}.to_json
   end
 end
